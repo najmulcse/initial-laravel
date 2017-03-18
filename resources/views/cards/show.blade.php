3aca75b0f6db6d3c83e119c02d89ea34fda1756d
Najmul Ahmed <najmul2022@gmail.com>
2017-03-18 10:47:15 +0600

    Show class is modified first time

--- resources/views/cards/show.blade.php
+++ resources/views/cards/show.blade.php
@@ -9,14 +9,16 @@
               <div class="col-sm-3"></div>
               <div class="col-sm-6 col-sm-offset-6">
                   <form method="POST" action="/card/{{$card->id}}/notes">
-                       {{csrf_field()}}
+
+                      {{csrf_field()}}
+
                           <ul>
                         @foreach($card->notes as $note)
                             <li class="form-control"> {{$note->body}}</li>
                         @endforeach
                           </ul>
                       <div class="form-group">
-                          <input class="form-control" name="note_body">
+                          <input class="form-control " type="text" name="note_body">
                       </div>
                       <div class="form-group">
                           <button class="btn btn-primary">Add note</button>
